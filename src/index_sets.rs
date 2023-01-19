9bda48e1be5fc4729742276c7aa8502a30817094
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2023-01-19 17:01:33 +0100

    Remove unused trait

--- src/index_sets.rs
+++ src/index_sets.rs
@@ -1,12 +1,3 @@
-pub trait IndexSet {
-    fn get_one_neighbor(&self) -> Option<(i64, Self)>
-    where
-        Self: Sized;
-    fn get_two_neighbor(&self) -> Option<(i64, Self)>
-    where
-        Self: Sized;
-}
-
 #[derive(Debug)]
 pub struct DenseState<'a> {
     shape: &'a Vec<usize>,
@@ -132,49 +123,6 @@ impl<'a> Iterator for DenseIterator<'a> {
     }
 }
 
-impl<'a> IndexSet for DenseState<'a> {
-    fn get_one_neighbor(&self) -> Option<(i64, Self)> {
-        // This is wrong!
-        let mut carry = 1;
-        let mut indices = self.indices.clone();
-
-        if carry == 1 {
-            return None;
-        }
-
-        for i in (0..self.indices.len()).rev() {
-            indices[i] = (self.indices[i] + carry) % self.shape[i];
-            carry = (self.indices[i] + carry) / self.shape[i];
-        }
-
-        if carry != 0 {
-            return None;
-        }
-
-        Some((1, DenseState::new_from_indices(indices, self.shape)))
-    }
-
-    fn get_two_neighbor(&self) -> Option<(i64, Self)> {
-        // This is wrong!
-        let mut carry = 1;
-        let mut indices = self.indices.clone();
-        if carry == 1 {
-            return None;
-        }
-
-        for i in (0..self.indices.len()).rev() {
-            indices[i] = (self.indices[i] + carry) % self.shape[i];
-            carry = (self.indices[i] + carry) / self.shape[i];
-        }
-
-        if carry != 0 {
-            return None;
-        }
-
-        Some((1, DenseState::new_from_indices(indices, self.shape)))
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -218,23 +166,6 @@ mod tests {
                     counter += 1;
                 }
             }
-        }
-    }
-
-    #[test]
-    fn test_get_one_neighbor_dense() {
-        let shape = vec![2, 2, 4, 5];
-
-        let state = DenseState::new_init_state(&shape);
-
-        loop {
-            let (sign, neighbor_state) = match state.get_one_neighbor() {
-                Some((s, n)) => (s, n),
-                None => break,
-            };
-
-            assert!(sign == 1);
-            // let mut diff
         }
     }
 
